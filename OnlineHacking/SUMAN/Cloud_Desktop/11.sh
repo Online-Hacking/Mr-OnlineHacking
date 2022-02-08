4112b8e154d8ae775d78c8cfc7a97927543fbc44
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-02-08 12:02:12 +0530

    Add files via upload

---
+++ OnlineHacking/SUMAN/Cloud_Desktop/11.sh
@@ -0,0 +1,42 @@
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing Free Cloud Desktop Please Wait....  \e[0m"
+echo -e ""
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!             KEY REQUIREMENTS         !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
+echo ""
+echo -e ""
+echo -e "\e[33m
+██╗  ██╗███████╗██╗   ██╗
+██║ ██╔╝██╔════╝╚██╗ ██╔╝
+█████╔╝ █████╗   ╚████╔╝
+██╔═██╗ ██╔══╝    ╚██╔╝
+██║  ██╗███████╗   ██║
+╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Generator License Key (free) \e[0m"
+echo ""
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://bit.ly/Cloud-RDP-License  "
+echo ""
+echo ""
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter License Active Key : \e[0m' option
+echo""
+echo""
+echo""
+if [[ $option == *'bda90dbc890b5f2923ceffe10d337'* ]]; then
+clear
+echo ""
+echo ""
+cd RDP
+apt install wget -y
+wget https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/Debian-Linux-ChomeDesktop.sh
+wget https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/ubuntu-desktop-lxde-vnc.sh
+cd ..
+echo ""
+echo
+fi
+sleep 3
+echo
+exit
