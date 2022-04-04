51eece4ee973d0ce83bc72961d99a58522d476e0
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-04-04 20:45:15 +0530

    Update Linux-Setup.sh

--- OnlineHacking/SUMAN/PUBG-BGMI/SM/OH/Linux-Setup.sh
+++ OnlineHacking/SUMAN/PUBG-BGMI/SM/OH/Linux-Setup.sh
@@ -83,7 +83,7 @@ read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m' opt
 echo""
 echo""
 echo""
-if [[ $option == *'abcdef12b478e48256775a4b69d65a'* ]]; then
+if [[ $option == *'abcdeff12b476e48256775a4d69b65a'* ]]; then
 clear
 echo ""
 echo ""
