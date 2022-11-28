148f4a0474fd188d1a1066ed2cdf3b5979412c62
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2022-11-28 19:11:02 +0530

    Update .Linux-Setup.sh

--- OnlineHacking/SUMAN/FreeFire/SM/Files/.Linux-Setup.sh
+++ OnlineHacking/SUMAN/FreeFire/SM/Files/.Linux-Setup.sh
@@ -254,12 +254,15 @@ echo -e "\e[33m
       ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
 echo ""
 echo ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Copy Activation Key \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[92m Activation Key 100% Free. Dont Buy  \e[34m( \e[1;36mplay video tutorial \e[34m) \e[0m"
+echo ""
+echo ""
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Shot Link and Generate Key and Copy Paste Activation Key \e[0m"
 echo ""
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m https://tinyurl.com/Free-Fire-Key  "
 echo ""
 echo ""
-read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Click Enter to Continue : \e[0m' option
+read -p $' \e[1;40m\e[31m[\e[32m*\e[31m]\e[32m Enter Activation Key : \e[0m\e[97m' option
 echo""
 echo""
 echo""
