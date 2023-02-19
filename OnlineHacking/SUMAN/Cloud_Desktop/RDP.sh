2d589b53c852098110336f6aae807c3a3600d07a
SUMAM MONDAL <63942767+Online-Hacking@users.noreply.github.com>
2023-02-19 20:04:19 +0530

    SUMAN

--- OnlineHacking/SUMAN/Cloud_Desktop/RDP.sh
+++ OnlineHacking/SUMAN/Cloud_Desktop/RDP.sh
@@ -16,7 +16,7 @@ echo -e "\e[93m
 ╚═╝  ╚═╝╚══════╝   ╚═╝    \e[0m\n"
 echo ""
 echo ""
-echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Website Scroll Down and Generator License Key (free) \e[0m"
+echo -e " \e[1;33m[\e[0m\e[1;77m~\e[0m\e[1;33m]\e[0m\e[1;32m \e[95m Visit This Website Scroll Down and Generator License Key (Free) \e[0m"
 echo ""
 echo -e " \e[91m[\e[92m*\e[91m]\e[1;93m Link :\e[0m\e[1;36m http://tinyurl.com/RDP-Linux-Key  "
 echo ""
@@ -31,16 +31,16 @@ echo ""
 echo ""
 cd RDP
 sudo apt install wget -y
-wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/Debian-Linux-ChomeDesktop.sh > /dev/null 2>&1
-wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/ubuntu-desktop-lxde-vnc.sh > /dev/null 2>&1
-wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/kali-linux.sh > /dev/null 2>&1
+wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/.Debian-Linux-ChomeDesktop.sh > /dev/null 2>&1
+wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/.ubuntu-desktop-lxde-vnc.sh > /dev/null 2>&1
+wget -q https://raw.githubusercontent.com/Online-Hacking/Mr-OnlineHacking/main/OnlineHacking/SUMAN/Cloud_Desktop/.kali-linux.sh > /dev/null 2>&1
 cd ..
 cd Docker-Ubuntu
-wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/nomachine-mate.sh > /dev/null 2>&1
-wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/nomachine-windows10.sh > /dev/null 2>&1
-wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/nomachine-wine.sh > /dev/null 2>&1
-wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/nomachine-xfce4.sh > /dev/null 2>&1
-wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/nomachine-kalilinux.sh > /dev/null 2>&1
+wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/.nomachine-mate.sh > /dev/null 2>&1
+wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/.nomachine-windows10.sh > /dev/null 2>&1
+wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/.nomachine-wine.sh > /dev/null 2>&1
+wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/.nomachine-xfce4.sh > /dev/null 2>&1
+wget -q https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/Cloud_Desktop/Docker-Ubuntu/.nomachine-kalilinux.sh > /dev/null 2>&1
 cd ..
 echo ""
 clear
